462092585de10e1b5dd00b837557293209ac19d6
domse <Dominik@Dominiks-MacBook-Pro.local>
2016-01-12 23:56:00 +0100

    json compatible
    Made the output json compatible and optimized internal names

--- pic.py
+++ pic.py
@@ -1,14 +1,15 @@
 from bottle import route, run, template, static_file
 from glob import glob
+import json
 
 @route('/')
 def index():
-	res = ''
-	list = glob("./data/picture/*.JPG")
-	for obj in list:
-		res += obj[15:] + ' '
-	return res
-	
+	picset = set()
+	picturelist = glob("./data/picture/*.jpg")
+	for picture in picturelist:
+		picset.add('/image/' + picture[15:])
+	return json.dumps(list(picset))
+
 @route('/image/<filename:re:.*\.jpg>')
 def image(filename):
 	return static_file(filename, root='./data/picture/', mimetype='image/jpg')
