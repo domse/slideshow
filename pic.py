adb8ec9491edf0d87cf76a9416aaf39ebb651a90
unknown <d.buecker@nbdbuecker.internal.iok.net>
2016-01-12 18:55:20 +0100

    Initial Commit

---
+++ pic.py
@@ -0,0 +1,20 @@
+from bottle import route, run, template, static_file
+from glob import glob
+
+@route('/hello/<name>')
+def index(name):
+    return template('<b>Hello {{name}}</b>!', name=name)
+
+@route('/')
+def index():
+	res = ''
+	list = glob("./data/picture/*.JPG")
+	for obj in list:
+		res += obj[15:] + ' '
+	return res
+	
+@route('/image/<filename:re:.*\.jpg>')
+def image(filename):
+	return static_file(filename, root='./data/picture/', mimetype='image/jpg')
+
+run(host='localhost', port=8080)
